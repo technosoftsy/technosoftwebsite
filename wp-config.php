39dd8eebdc1960b5f6d1ec7d56e8e8c2f3a301a0
abdlwahhab <abdlwahhab@gmail.com>
2022-07-22 10:37:26 +0300

    correct db_name

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost1' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
