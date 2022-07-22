ae752c9e5f755db669d6b48c14408ffb97c9bd04
TechnoSoft <technosoftsy@gmail.com>
2022-07-22 10:06:26 +0300

    second commit

--- New folder/wp-content/themes/cozipress/sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! is_active_sidebar( 'cozipress-sidebar-primary' ) ) {	return; } ?>
-<div class="col-lg-4 pl-lg-4">
-	<div class="sidebar">
-		<?php dynamic_sidebar('cozipress-sidebar-primary'); ?>
-	</div>
-</div>
