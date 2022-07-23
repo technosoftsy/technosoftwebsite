b109e483c8eec215d7963535cf644a4a90b509ed
TechnoSoft <technosoftsy@gmail.com>
2022-07-23 17:02:58 +0300

    wp config  locally and website test

--- wp-config.php
+++ wp-config.php
@@ -18,7 +18,7 @@
  * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
+// ** Database settings Haldun  - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 
 
